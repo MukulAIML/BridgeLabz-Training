ffd74d7a06458e1aaedd2a21ef15dd72d915a155
MukulAIML <mukul.verma_cs.aiml22@gla.ac.in>
2025-12-19 11:06:44 +0530

    Rename Java files to clean naming format

--- core-java-practice/gcr-codebase/programming-elements/Q1(Harry'sAge).java
+++ core-java-practice/gcr-codebase/programming-elements/Q1(Harry'sAge).java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Q1(Harry'sAge).{
+public class Q1(Harry'sAge){
  public static void main(String[] args){
    int age = 2024-2000-1;
    System.out.print(" Harry's age in 2024 is "+age);
