ecb880a72cb5f4957bf54e88ffd6bae2cc1eeb90
MukulAIML <mukul.verma_cs.aiml22@gla.ac.in>
2026-01-27 15:12:11 +0530

    [Mukul] Add.Adding CSV solutions

---
+++ gcr-codebase/CSVDataHandeling/CSVtoJava2.java
@@ -0,0 +1,8 @@
+List<Student> students = new ArrayList<>();
+students.add(new Student(
+    Integer.parseInt(row[0]),
+    row[1],
+    Integer.parseInt(row[2]),
+    Integer.parseInt(row[3])
+));
+
